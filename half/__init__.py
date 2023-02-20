524ff82b1a66d65b6c552269e13feb917b2dda12
Marcus T <mtwyford@users.noreply.github.com>
2023-02-19 21:20:09 -0500

    Create __init__.py

---
+++ half/__init__.py
@@ -0,0 +1,24 @@
+import check50
+import check50.c
+import re
+
+@check50.check()
+def exists():
+    """half.c exists"""
+    check50.exists("half.c")
+
+@check50.check(exists)
+def compiles():
+    """half.c compiles"""
+    # Check if student code compiles
+    check50.c.compile("half.c", lcs50=True)
+    
+    # Rename main function to "distro_main"
+    half = re.sub("int\s+main", "int distro_main", open("half.c").read())
+
+    
+@check50.check(compiles)
+def encrypts_a_as_b():
+    """Prints correct with 12.50 bill, 8.875\% tax, 20\% tip"""
+    check50.run("./half").stdin(12.50, 8.875, 20).stdout("You will owe $\i*8.17 each!, "You will owe $8.17 each!\n").exit(0)
+
